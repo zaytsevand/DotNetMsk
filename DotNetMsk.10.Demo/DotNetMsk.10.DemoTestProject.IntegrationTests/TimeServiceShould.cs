955ea9571a712841f858737b36f582dc9470b479
Andrey Zaytsev <azaytsev@usetech.ru>
2017-05-11 19:21:06 +0300

    Basic test data

--- DotNetMsk.10.Demo/DotNetMsk.10.DemoTestProject.IntegrationTests/TimeServiceShould.cs
+++ DotNetMsk.10.Demo/DotNetMsk.10.DemoTestProject.IntegrationTests/TimeServiceShould.cs
@@ -1,9 +1,25 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Threading.Tasks;
+using DotNetMsk._10.DemoTestProject.Tests.Shared;
+using NUnit.Framework;
 
 namespace DotNetMsk._10.DemoTestProject.IntegrationTests
 {
     [TestFixture]
     public class TimeServiceShould
     {
+        [Severity(Severity.Blocker)]
+        [TestCase("2017-01-01", ExpectedResult = "")]
+        [TestCase("1970-01-01T00:00:00.0000000Z", ExpectedResult = "00:00:00")]
+        [TestCase("1970-01-01T00:00:00.0000001Z", ExpectedResult = "00:00:00.0000001")]
+        [TestCase("1969-12-31T23:59:59.9999999Z", ExpectedResult = "-00:00:00.0000001")]
+
+        [TestCase("9999-12-31T20:59:59.9999999Z", ExpectedResult = "2932896.17:59:59.9999999")]
+
+        [TestCase("0001-01-01T00:00:00.0000000Z", ExpectedResult = "-719162.03:00:00")]
+        public Task CalculateTimestampFromStartOfUnixEpoch(string tillDate)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- DotNetMsk.10.Demo/DotNetMsk.10.DemoTestProject.UnitTests/TimerClientShould.cs
+++ DotNetMsk.10.Demo/DotNetMsk.10.DemoTestProject.UnitTests/TimerClientShould.cs
@@ -16,8 +16,19 @@ namespace DotNetMsk._10.DemoTestProject.UnitTests
             _httpMock = HttpMockServer.LaunchTimeStub("http://localhost:10001/api/time");
         }
 
-        [Test, Severity(Severity.Blocker)]
-        public Task CalculateTimestampFromStartOfUnixEra() { throw new NotImplementedException(); }
+        [Severity(Severity.Blocker)]
+        [TestCase("2017-01-01", ExpectedResult = "")]
+        [TestCase("1970-01-01T00:00:00.0000000Z", ExpectedResult = "00:00:00")]
+        [TestCase("1970-01-01T00:00:00.0000001Z", ExpectedResult = "00:00:00.0000001")]
+        [TestCase("1969-12-31T23:59:59.9999999Z", ExpectedResult = "-00:00:00.0000001")]
+
+        [TestCase("9999-12-31T20:59:59.9999999Z", ExpectedResult = "2932896.17:59:59.9999999")]
+
+        [TestCase("0001-01-01T00:00:00.0000000Z", ExpectedResult = "-719162.03:00:00")]
+        public Task CalculateTimestampFromStartOfUnixEpoch(string tillDate)
+        {
+            throw new NotImplementedException();
+        }
 
         public void Dispose()
         {
